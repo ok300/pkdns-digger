fc20966923253045bf7e5d2cf571bd73e06fb3a5
tipogi <tipogi@pm.me>
2025-07-24 16:21:57 +0200

    change nextjs config for static file bundle

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export', 
   webpack: (config, { isServer }) => {
     // Handle WASM files
     config.experiments = {
